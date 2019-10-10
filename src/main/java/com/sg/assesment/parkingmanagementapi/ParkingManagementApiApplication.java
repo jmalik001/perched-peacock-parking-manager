aa41b7d709f4ebd3f5da26821c2fd74c24d7ec0a
jmalik001 <jagabandhumallick@gmail.com>
2019-10-10 18:17:46 +0530

    initial commit with project structure and dependency

---
+++ src/main/java/com/sg/assesment/parkingmanagementapi/ParkingManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.sg.assesment.parkingmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sg/assesment/parkingmanagementapi/ParkingManagementApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sg.assesment.parkingmanagementapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParkingManagementApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
