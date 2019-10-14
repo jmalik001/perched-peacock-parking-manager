9c5f18abe9736b922f0806162215542eba9e0b4d
jmalik001 <jagabandhumallick@gmail.com>
2019-10-14 21:39:15 +0530

    UI clean up

---
+++ perched-peacock-parcking-gui/src/app/routes/search-parking/search-parking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ci-search-parking',
+  templateUrl: './search-parking.component.html',
+  styleUrls: ['./search-parking.component.scss']
+})
+export class SearchParkingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ perched-peacock-parcking-gui/src/app/routes/search-parking/search-parking.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchParkingComponent } from './search-parking.component';
+
+@NgModule({
+  declarations: [SearchParkingComponent],
+  imports: [CommonModule],
+exports: [SearchParkingComponent]
+})
+export class SearchParkingModule {}
